c01a69a82daf35cdafd4d39ac004feb7b7b325b4
Nurul Huda <hudasanca@gmail.com>
2016-11-15 21:33:24 +0700

    add user from social media if they dont exist

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\User;
 use Socialite;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -16,7 +17,18 @@ class AuthController extends Controller
 
     public function handleProviderCallback(Request $request, $provider)
     {
-    	$user = Socialite::driver($provider)->user();
+    	$userFromProvider = Socialite::driver($provider)->user();
+
+    	# trying to login
+    	$user = User::email($userFromProvider->email)->first();
+
+    	if (!$user) {
+    		$user = User::create([
+    			'name' => $userFromProvider->name,
+    			'email' => $userFromProvider->email,
+    			'username' => $userFromProvider->nickname
+    		]);
+    	}
 
     	return response()->json($user);
     }
