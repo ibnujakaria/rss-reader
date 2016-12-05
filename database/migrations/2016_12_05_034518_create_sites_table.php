03d9d6ff27359ec7b2aa695835a4b2edfb81db66
Nurul Huda <hudasanca@gmail.com>
2016-12-05 10:57:37 +0700

    create table schema

---
+++ database/migrations/2016_12_05_034518_create_sites_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSitesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sites', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 50);
+            $table->string('url')->unique();
+            $table->string('feed_url')->unique();
+            $table->string('description');
+            $table->date('last_synced');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sites');
+    }
+}
